861e34586565235598721a82db24923a21194202
mr-ema <ema.a.v.c2017@gmail.com>
2024-11-19 22:20:37 -0300

    feat(build): add ESM support

--- rollup.config.js
+++ rollup.config.js
@@ -2,23 +2,38 @@ import terser from "@rollup/plugin-terser";
 
 const build = {
     input: "./src/onepage.js",
-    output: {
-        file: "./dist/onepage.js",
-        format: "iife",
-        name: "Onepage",
-        sourcemap: false,
-    },
+    output: [
+        {
+            file: "./dist/onepage.js",
+            format: "iife",
+            name: "Onepage",
+            sourcemap: false,
+        },
+        {
+            file: "./dist/onepage.esm.js",
+            format: "esm",
+            sourcemap: false,
+        },
+    ],
 }
 
 const minifiedBuild = {
     input: "./src/onepage.js",
-    output: {
-        file: "./dist/onepage.min.js",
-        format: "iife",
-        name: "Onepage",
-        compact: true,
-        sourcemap: false,
-    },
+    output: [
+        {
+            file: "./dist/onepage.min.js",
+            format: "iife",
+            name: "Onepage",
+            compact: true,
+            sourcemap: false,
+        },
+        {
+            file: "./dist/onepage.esm.min.js",
+            format: "esm", // Minified ES module version
+            compact: true,
+            sourcemap: false,
+        },
+    ],
 
     plugins: [terser()],
 };
